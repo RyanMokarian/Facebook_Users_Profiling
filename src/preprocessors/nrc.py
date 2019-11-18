b3433a1dd3263e1e00a2a5bf0c78f1827d42f300
MShahi, Maziar <maziar_mshahi@manulife.com>
2019-11-17 21:56:29 -0500

    linear regression on personality traits

--- src/preprocessors/nrc.py
+++ src/preprocessors/nrc.py
@@ -1,19 +1,17 @@
-from scipy.stats import chi2
-from sklearn.ensemble import ExtraTreesClassifier
-from sklearn.feature_selection import SelectKBest
 from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error
-from sklearn.model_selection import cross_val_score, train_test_split
-from sklearn.svm import LinearSVC
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+import pickle
 
 from src.util import Utils
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
 
 pd.set_option('display.max_rows', 500)
 pd.set_option('display.max_columns', 500)
 pd.set_option('display.width', 1000)
+
 
 def get_data():
     util = Utils()
@@ -22,36 +20,24 @@ def get_data():
     nrc_df.rename(columns={'userId': 'userid'}, inplace=True)
     merged_df = pd.merge(nrc_df, profile_df, on='userid')
     return merged_df.filter(['positive', 'negative', 'anger', 'anticipation', 'disgust',
-                             'fear', 'joy', 'sadness', 'surprise', 'trust', 'ope'], axis=1)
+                             'fear', 'joy', 'sadness', 'surprise', 'trust', 'ope', 'con', 'ext', 'agr', 'neu'], axis=1)
+
 
 if __name__ == '__main__':
     df = get_data()
 
-    df.drop(['negative', 'anticipation', 'fear', 'joy', 'sadness'], axis=1, inplace=True)
-    X = df.iloc[:, 0:-1]  # independent columns
+    # df.drop(['negative', 'anticipation', 'fear', 'joy', 'sadness'], axis=1, inplace=True)
+    X = df.iloc[:, 0:-5]  # independent columns
     print(X.describe())
     X = np.log(X + 1)
     X=(X-X.min())/(X.max()-X.min())
     X.hist()
     plt.show()
     print(X.describe())
-    y = df.iloc[:, -1]  # target column i.e price range
-
-    import statsmodels.api as sm
-    from scipy import stats
-
-    mod = sm.OLS(y, X)
-    fii = mod.fit()
-    print(fii.summary2())
-
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+    y = df[df.columns[-5:]]
 
     reg = LinearRegression()
 
-    # scores = cross_val_score(reg, X, y, cv=5)
-    reg.fit(X_train, y_train)
-    predict = reg.predict(X_test)
-    print(np.sqrt(np.average((y_test-predict)**2))) # this is their method of error calculation
-
-    # This will print the mean of the list of errors that were output and
-    # provide your metric for evaluation
+    reg.fit(X, y)
+    pickle.dump(reg, open("../resources/LinearReg_Personality.sav", 'wb'))
+    # print(np.sqrt(np.average((y_test - predict) ** 2, axis=0)))
